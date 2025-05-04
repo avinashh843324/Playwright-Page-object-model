b4b700246ed4d6cbf0e4d50c6b618f8b640a50f4
avinash kumar <avinashh7777@gmail.com>
2025-05-05 01:44:12 +0530

    file upload- download handling

--- tests/fileUpload.spec.js
+++ tests/fileUpload.spec.js
@@ -1,8 +1,33 @@
-import test from "playwright/test";
+import {test,expect} from'@playwright/test';
+import path from 'path';
+7
+test("File upload testing",async({page})=>{
+    await page.goto("https://practice-automation.com/file-upload/");
+    
+    //method 1 using __dirname → gives you the full path to test & path.resolve(...) → converts it to an absolute path that Playwright can use
+    // const filepath = path.resolve(__dirname,'../utils/PNG1.png'); //../utils/PNG1.png' → goes up one level (..) to reach utils/
+    // await page.locator("//input[@id='file-upload']").setInputFiles(filepath);
 
-test(" Upload a single image file and verify it appears in the grid",async({page}) =>{
-await page.goto("file:///C:/Users/avina/index.html");
-const a =  page.locator("//input[@id='fileInput']");
-// let b =  PNG1.resolve(__dirname,"utils\test-data\PNG1.png")
-await a.setInputFiles("utils/PNG1.png");
+    //Method 2 direct
+    
+    await page.locator("//input[@id='file-upload']").setInputFiles("./utils/PNG1.png")
+    await page.locator("input[type='submit']").click();
+    const text = await page.locator("//div[contains(text(),'Thank you for your message. It has been sent.')]").textContent();
+    expect(text).toBe("Thank you for your message. It has been sent.");
+
+})
+
+test.only("file download testing",async({page})=>{
+    await page.goto("https://www.tutorialspoint.com/selenium/practice/upload-download.php");
+
+    const [download] = await Promise.all([
+        page.waitForEvent('download'),
+        page.click('#downloadButton')
+    ]);
+
+    const filename = download.suggestedFilename(); // e.g., 'sampleFile.txt'
+  const filepath = path.join('./utils/downloads', filename);
+
+  await download.saveAs(filepath);
+
 })
